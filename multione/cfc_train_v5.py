71c1264e3c80a2aa2f57d4aa0145b66eea6563a8
Josip Križan <jkrizan@multione.hr>
2025-09-01 10:00:27 +0200

    Hydra scaler

--- multione/cfc_train_v5.py
+++ multione/cfc_train_v5.py
@@ -21,6 +21,7 @@ import torch
 
 #from cfc_dataset import ArcoV2DatasetV2
 from cfc_v5_4 import CfcModel_v5, CfcLearner_v5
+import pickle
 
 # Tensor cores:
 # https://medium.com/@michael.diggin/the-power-of-8-getting-the-most-out-of-tensor-cores-c7704ae0c5c1
@@ -169,6 +170,47 @@ def train_test_v5_4():
                          precision='16-mixed') #, devices=[0,1])
     trainer.fit(learner) #, train_loader, val_loader)
 
+def train_test_v5_5():
+    input_size = 8 #9
+    output_size = 6 #7
+    sequence_length=12
+    years = np.arange(2000, 2024)
+    fn_zarr = Path(f"/home/josip/arcov2/sample_v1.zarr")
+
+    learner = CfcLearner_v5(fn_zarr, 
+                            years, 
+                            input_size, 
+                            hidden_size=128, 
+                            sequence_length=sequence_length, 
+                            output_size=output_size,
+                            backbone_layers=[128,64,32,16],
+                            limit=None, 
+                            activation='relu',  ##silu, relu, tanh, gelu, lecun_tanh
+                            lr=0.01,
+                            debug=False)
+    
+    '''
+    learner.setup()
+    y, x, timespans = next(iter(learner.train_dataloader()))
+    y= y.to(torch.float32); x=x.to(torch.float32); timespans = timespans.to(torch.float32)
+    loss = learner.training_step((y, x, timespans),0)
+    '''
+
+    #torch.multiprocessing.set_start_method('spawn')
+    #torch.set_float32_matmul_precision('medium')
+    checkpoint_callback = ModelCheckpoint(
+        # dirpath=checkpoints_path, # <--- specify this on the trainer itself for version control
+        filename="cfc_v5.5_e{epoch:03d}_{val_loss:.2f}",
+        every_n_epochs=1,
+        save_top_k=-1,  # <--- this is important!
+    )
+    trainer = pl.Trainer(max_epochs=300,
+                         callbacks=[checkpoint_callback],
+                         num_nodes=1, #devices=[0,1,2],
+                         precision='16-mixed') #, devices=[0,1])
+    trainer.fit(learner) #, train_loader, val_loader)
+
+
 def train_test_v5_2_continue():
     input_size = 8 #9
     output_size = 6 #7
@@ -230,7 +272,7 @@ def train_test_v4_continue():
     trainer.fit(learner, ckpt_path="/home/josip/scikit-map/multione/lightning_logs/version_22/checkpoints/cfc-v4_e-99.ckpt") #, train_loader, val_loader)
 
 if __name__=="__main__":
-    train_test_v5_4()
+    train_test_v5_5()
     #train_test_v5_2_continue()
     #train_test_v5_1_continue()
     #train_test_v4_continue()
