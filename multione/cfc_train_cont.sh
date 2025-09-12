ddf6afe610971a38cd25e9c43ef897d1c1683fde
Josip Križan <jkrizan@multione.hr>
2025-09-12 17:42:46 +0200

    Hydra

--- multione/cfc_train_cont.sh
+++ multione/cfc_train_cont.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-#python cfc_train_v6.py lightning_logs/version_15/checkpoints/cfc_v6_b2_epoch-093.ckpt
-#python cfc_train_v6.py lightning_logs/version_16/checkpoints/cfc_v6_b3_epoch-089.ckpt
-#python cfc_train_v6.py lightning_logs/version_18/checkpoints/cfc_v6_b5_epoch-095.ckpt
-python cfc_train_v6.py 0 0 1 2 3
-python cfc_train_v6.py 2 0 1 2 3
-python cfc_train_v6.py 3 0 1 2 3
-python cfc_train_v6.py 4 0 1 2 3
-python cfc_train_v6.py 5 0 1 2 3
+python cfc_train_v6.py /mnt/nibble/gen_cog/arcov2/v6/checkpoints/cfc_v6_b0_epoch-021.ckpt
+python cfc_train_v6.py /mnt/nibble/gen_cog/arcov2/v6/checkpoints/cfc_v6_b1_epoch-015.ckpt
+python cfc_train_v6.py /mnt/nibble/gen_cog/arcov2/v6/checkpoints/cfc_v6_b2_epoch-007.ckpt
+python cfc_train_v6.py /mnt/nibble/gen_cog/arcov2/v6/checkpoints/cfc_v6_b3_epoch-022.ckpt
+python cfc_train_v6.py /mnt/nibble/gen_cog/arcov2/v6/checkpoints/cfc_v6_b4_epoch-023.ckpt
+python cfc_train_v6.py /mnt/nibble/gen_cog/arcov2/v6/checkpoints/cfc_v6_b5_epoch-009.ckpt
+
+
+# #python cfc_train_v6.py 0 0 1 2 
+# python cfc_train_v6.py 1 0 1 2 
+# #python cfc_train_v6.py 2 0 1 2 
+# python cfc_train_v6.py 3 0 1 2 
+# python cfc_train_v6.py 4 0 1 2 
+# python cfc_train_v6.py 5 0 1 2 
--- multione/cfc_train_v6.py
+++ multione/cfc_train_v6.py
@@ -97,7 +97,7 @@ def train_v6(band, devices):
 #%%
 def train_v6_continue(checkpoint_path:str):
     band = int(Path(checkpoint_path).stem.split('_')[2][1:])
-    devices=[0,1,2,3]
+    devices=[0,1,2]
     input_size = 3 #8
     timeless_size = 3
     output_size = 1 #6 #7
@@ -136,7 +136,7 @@ def train_v6_continue(checkpoint_path:str):
     checkpoint_callback.CHECKPOINT_NAME_LAST = f"cfc_v6_b{band}_last"    
     checkpoint_callback.CHECKPOINT_EQUALS_CHAR = "-"
 
-    import os
+    #import os
     #os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
     # os.environ["TORCH_USE_CUDA_DSA"] = "1"
 
@@ -152,17 +152,17 @@ if __name__=="__main__":
     #train_v6_continue()
     
     
-    import sys 
-    band = int(sys.argv[1])
-    devices = [int(x) for x in sys.argv[2:]]
-    print(f"Training band {band} on devices {devices}")
-    train_v6(band, devices)
+    # import sys 
+    # band = int(sys.argv[1])
+    # devices = [int(x) for x in sys.argv[2:]]
+    # print(f"Training band {band} on devices {devices}")
+    # train_v6(band, devices)
     
     
-    # import sys
-    # checkpoint_path = sys.argv[1]
-    # print(f"Continuing training from checkpoint {checkpoint_path}")
-    # train_v6_continue(checkpoint_path)
+    import sys
+    checkpoint_path = sys.argv[1]
+    print(f"Continuing training from checkpoint {checkpoint_path}")
+    train_v6_continue(checkpoint_path)
 
     #train_test_v5_2_continue()
     #train_test_v5_1_continue()
